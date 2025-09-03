23152e5354b5afb7de579515975d088ea5c50a13
Jeffrey Zhu <JeffreyZhu0201@gmail.com>
2025-09-03 22:15:04 +0800

    完成注册页

--- GardenGuideAI/app/RegisterPage.tsx
+++ GardenGuideAI/app/RegisterPage.tsx
@@ -2,24 +2,26 @@
  * @Author: Jeffrey Zhu JeffreyZhu0201@gmail.com
  * @Date: 2025-09-01 01:27:08
  * @LastEditors: Jeffrey Zhu JeffreyZhu0201@gmail.com
- * @LastEditTime: 2025-09-02 16:50:27
+ * @LastEditTime: 2025-09-03 22:14:24
  * @FilePath: /GardenGuideAI/GardenGuideAI/app/RegisterPage.tsx
  * @Description: 登陆页面
  * 
  * Copyright (c) 2025 by Jeffrey Zhu, All Rights Reserved. 
  */
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
-import {RootStackParamList} from "./(tabs)/mine"
+import { View, Text, TextInput, Button, StyleSheet, Pressable } from 'react-native';
+import { RootStackParamList } from "./(tabs)/mine"
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { useNavigation, useFocusEffect, ThemeContext } from "@react-navigation/native";
 import useStore from './store/store';
 import { User } from '@/constants/User';
-// import { useCameraPermissions } from 'expo-camera';
-import { Login } from "../network/userApi"
+import { useCameraPermissions } from 'expo-camera';
+import { Login, Register } from "../network/userApi"
+import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
 
 
-export default function LoginPage() {
+export default function RegiterPage() {
   const { userInfo, setUserInfo, token, setToken } = useStore()
 
   const [currentUserEmail, setCurrentUserEmail] = useState<string>("")
@@ -27,10 +29,11 @@ export default function LoginPage() {
 
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList, 'RegisterPage'>>();
 
+
   const handleLogin = async (Email: string, Password: string) => {
     // Implement your login logic here
     console.log('Logging in with:', Email, Password);
-    const response = await Login(Email, Password)
+    const response = await Register(Email, Password)
 
     if (response.code == 200 && response.data) {
       console.log(response.data.token);
@@ -39,26 +42,25 @@ export default function LoginPage() {
         email: Email,
         password: Password
       })
-      console.log("Logged in successfully")
-      alert("Logged in successfully")
+      alert("Registered in successfully")
       await new Promise(resolve => setTimeout(resolve, 1000));
       navigation.goBack()
-      // navigation.navigate('Mine')
-    }else{
+    } else {
       alert(response.message)
       navigation.goBack()
-      // navigation.navigate('Mine')
     }
   }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
+      <Text style={styles.title}>Register</Text>
+      <Text style={styles.smallText}>Username:</Text>
       <TextInput
         style={styles.input}
         placeholder="Username"
         value={currentUserEmail}
         onChangeText={setCurrentUserEmail}
       />
+      <Text style={styles.smallText}>Password:</Text>
       <TextInput
         style={styles.input}
         placeholder="Password"
@@ -66,7 +68,9 @@ export default function LoginPage() {
         value={currentUserPassword}
         onChangeText={setCurrentUserPassword}
       />
-      <Button title="Login" onPress={() => handleLogin(currentUserEmail, currentUserPassword)} />
+      <Pressable onPress={() => handleLogin(currentUserEmail, currentUserPassword)} style={styles.buttonStyle}>
+          <ThemedText style={{color:"#ffffff",padding:12,fontSize:18,fontWeight:"600",alignItems:'center'}}>Register & Login</ThemedText>
+      </Pressable>
     </View>
   );
 };
@@ -74,20 +78,34 @@ export default function LoginPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     padding: 20,
+    backgroundColor: '#ffffffff',
   },
   title: {
-    fontSize: 24,
+    fontSize: 36,
+    fontWeight: "600",
     marginBottom: 20,
+  },
+  smallText:{
+    fontSize:14,
+    fontWeight:"600",
+    color:"#5c5c5cff"
   },
   input: {
     width: '100%',
-    padding: 10,
+    padding: 12,
     marginBottom: 10,
     borderWidth: 1,
     borderColor: '#ccc',
-    borderRadius: 5,
+    borderRadius: 10,
+    fontSize: 16,
+    fontWeight: "400"
   },
+  buttonStyle: {
+    borderRadius:20,
+    marginVertical:16,
+    display:'flex',
+    alignItems:'center',
+    backgroundColor:"#00528cff",
+  }
 });
